7d3edbb81065afe480cf677993072258833c9561
adriancarranza <adriancarranza0113@gmail.com>
2023-12-05 15:20:19 -0800

    Story 1 Complete

---
+++ db/migrate/20231205215508_create_animals.rb
@@ -0,0 +1,10 @@
+class CreateAnimals < ActiveRecord::Migration[7.0]
+  def change
+    create_table :animals do |t|
+      t.string :CommonName
+      t.string :ScientificBinomial
+
+      t.timestamps
+    end
+  end
+end
